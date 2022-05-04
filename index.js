9e09bc8bbe51076882076b6679e1d0e70f3b17b1
Susan Minott <susan_minott@yahoo.com>
2022-05-03 22:38:01 -0400

    commit tree d6f0e91d463917f36888ed40deaecb82acdd4098

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Your code here
+//1ST CODE
+function moveDodgerLeft(){
+    const dodger = document.getElementById('dodger');
+
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }
+};
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+//2ND CODE
+function moveDodgerRight(){
+    const dodger = document.getElementById('dodger');
+
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left + 1}px`;
+    }
+};
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
